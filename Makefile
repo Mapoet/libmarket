4d975ae9787283b2ab72d966628c4d4ac627d87f
deme <deme@debian.deme.es>
2018-03-02 23:03:35 +0100

    Modified Makefile

--- Makefile
+++ Makefile
@@ -1,24 +1,13 @@
 LIB = market
 
 CFLAGS = -Wall -rdynamic
-OBJECTS = $(subst src,obj,$(patsubst %.c,%.o,$(wildcard src/market/*.c))) \
-	$(subst src,obj,$(patsubst %.c,%.o,$(wildcard src/dm/*.c)))
+OBJECTS = $(subst src,obj,$(patsubst %.c,%.o,$(wildcard src/market/*.c)))
 
 lib/lib$(LIB).a : $(OBJECTS)
 	ar rcs lib/lib$(LIB).a $(OBJECTS)
 
 obj/market/%.o : src/market/%.c include/market/%.h
 	gcc $(CFLAGS) -c $< -o $@ \
-		-Iinclude \
+		-Iinclude -Ilib/libdmc/include \
+		-L../lib -l$(LIB) -Llib/libdmc -ldmc\
 		-lgc
-
-obj/dm/%.o : src/dm/%.c include/dm/%.h
-	gcc $(CFLAGS) -c $< -o $@ \
-		-Iinclude \
-		-lgc
-
-clean:
-	rm -fR obj/market
-	rm -fR obj/dm
-	mkdir obj/market
-	mkdir obj/dm
--- src/market/Quote.c
+++ src/market/Quote.c
@@ -2,24 +2,16 @@
 // GNU General Public License - V3 <http://www.gnu.org/licenses/>
 
 /*.+
-+struct:Quote
-  open: double: double
-  close: double: double
-  max: double: double
-  min: double: double
+struct:Quote
+  open: double: _double4
+  close: double: _double4
+  max: double: _double4
+  min: double: _double4
   vol: size_t: _uint
 */
 
 #include "market/Quote.h"
 #include "dm/dm.h"
-
-static Json *double_serialize(double n) {
-  return json_wdouble(n, 4);
-}
-
-static double double_restore(Json *serial) {
-  return json_rdouble(serial);
-}
 
 /*.-.*/
 struct quote_Quote {
@@ -73,10 +65,10 @@ size_t quote_vol(Quote *this) {
 
 Json *quote_serialize(Quote *this) {
   Arr/*Json*/ *serial = arr_new();
-  arr_add(serial, double_serialize(this->open));
-  arr_add(serial, double_serialize(this->close));
-  arr_add(serial, double_serialize(this->max));
-  arr_add(serial, double_serialize(this->min));
+  jarr_adouble(serial, this->open, 4);
+  jarr_adouble(serial, this->close, 4);
+  jarr_adouble(serial, this->max, 4);
+  jarr_adouble(serial, this->min, 4);
   jarr_auint(serial, this->vol);
   return json_warray(serial);
 }
@@ -85,10 +77,10 @@ Quote *quote_restore(Json *s) {
   Arr/*Json*/ *serial = json_rarray(s);
   Quote *this = MALLOC(Quote);
   size_t i = 0;
-  this->open = double_restore(arr_get(serial, i++));
-  this->close = double_restore(arr_get(serial, i++));
-  this->max = double_restore(arr_get(serial, i++));
-  this->min = double_restore(arr_get(serial, i++));
+  this->open = jarr_gdouble(serial, i++);
+  this->close = jarr_gdouble(serial, i++);
+  this->max = jarr_gdouble(serial, i++);
+  this->min = jarr_gdouble(serial, i++);
   this->vol = jarr_guint(serial, i++);
   return this;
 }
